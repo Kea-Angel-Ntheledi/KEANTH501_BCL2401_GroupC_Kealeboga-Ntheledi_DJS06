8a4d1a983e30b409087b3f68359912a9178ed6a7
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-05-24 13:30:07 +0200

    Debugged

--- scripts.js
+++ scripts.js
@@ -1,13 +1,42 @@
-import { provinces, names, products } from './data.js';
+// A list of provinces:
+const provinces = [
+  "Western Cape",
+  "Gauteng",
+  "Northern Cape",
+  "Eastern Cape",
+  "KwaZulu-Natal",
+  "Free State",
+];
+
+// A list of names:
+const names = [
+  "Ashwin",
+  "Sibongile",
+  "Jan_Hendrik",
+  "Sifso",
+  "Shailen",
+  "Frikkie",
+];
+
+// A list of products with prices:
+const products = [
+  { product: "banana", price: "2" },
+  { product: "mango", price: 6 },
+  { product: "potato", price: " " },
+  { product: "avocado", price: "8" },
+  { product: "coffee", price: 10 },
+  { product: "tea", price: "" },
+];
+
 // forEach Basics
 //log each name to the console
 names.forEach((name) => {
-  console.log(name)
+  console.log(name);
 });
 
 //log each province to the console
-names.forEach((province) => {
-  console.log(province)
+provinces.forEach((provinces) => {
+  console.log(provinces);
 });
 
 //log each name with a matching province in the format "Name (Province)"
